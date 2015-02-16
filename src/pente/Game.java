e46e688aab71723bb57f47c0ba8648c825c5ceaf
John Maloney <jmal0320@gmail.com>
2015-02-16 16:11:40 -0500

    Removed sleep so no one thinks its slow. Thanks Leo for pointing that out

--- src/pente/Game.java
+++ src/pente/Game.java
@@ -40,12 +40,6 @@ public class Game{
 			System.out.println();
 			
 			nextPlayer = (nextPlayer + 1)%players.length;
-
-			try{
-				Thread.sleep(1000);
-			} catch(InterruptedException ex){
-				Thread.currentThread().interrupt();
-			}
 		}
 
 		return board.getWinner();
